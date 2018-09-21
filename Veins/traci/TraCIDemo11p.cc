33070ac801b24fde62e6934967ea83ec264fe5aa
TerryLew <terrysatou@gmail.com>
2018-09-20 23:28:41 -0700

    clean 2

--- Veins/traci/TraCIDemo11p.cc
+++ Veins/traci/TraCIDemo11p.cc
@@ -35,78 +35,73 @@ void TraCIDemo11p::initialize(int stage) {
         currentSubscribedServiceId = -1;
     }
     hostSt.insert(myId);
-    
+    //choose route
     Veins::TraCIScenarioManager* manager = Veins::TraCIScenarioManagerAccess().get();
-        traci = manager->getCommandInterface();
-
-        double flow1 = traci->getLastStepVehicleNumber("flow1a")+traci->getLastStepVehicleNumber("flow1b");
-        double flow2 = traci->getLastStepVehicleNumber("flow2");
-
-        TraCICommandInterface::Road road1 = TraCICommandInterface::Road(traci, "edge1");
-        TraCICommandInterface::Road road2 = TraCICommandInterface::Road(traci, "edge2");
-
-        double rate = 6000.0, l1 = 1000, l2 = 500, v_max = 15.65, cap1 = 1500, cap2 = 1500, alpha=0.2, beta=10, lane1 = 2, lane2 = 1;
-        double weight = 0.2;
-        double c_max_1 = 150;
-        double c_max_2 = 150;
-
-        double r = 361.0;
-        double p = 1.0/20.0;
-
-        auto c = getCommunicationCost();
-        double c1 = c.first;
-        double c2 = c.second;
-
-        double v1 = road1.getMeanSpeed();
-        double v2 = road2.getMeanSpeed();
-        double t1 = l1/v1;
-        double t2 = l2/v2;
-        double x1 = flow1/l1*v1;
-        double x2 = flow2/l2*v2;
-
-        double J1 = weight*t1+(1-weight)*c1;
-        double J2 = weight*t2+(1-weight)*c2;
-
-        double systemCost = t1*x1+t2*x2;
-
-        output << simTime() <<" " << systemCost << "\n";
-        outputFlow << simTime() <<" "<<x1*3600<<"\n";
+    traci = manager->getCommandInterface();
     
-        double x, speed;
-        if(simTime() < 20) {
-            Mac1609_4 *mac = FindModule<Mac1609_4*>::findSubModule(getParentModule());
-            if(t1 > t2) {//choose route 1
-                traciVehicle->changeRoute("edge2", 9999);
-                double u = flow1/l1;
-                x = u*v1*3600;
-                mac->setParametersForBitrate(BITRATES_80211P[b1]);
-            } else {//choose route 2
-                traciVehicle->changeRoute("edge1", 9999);
-                double u = flow2/l2;
-                x = u*v2*3600;
-                speed = v_max/(1.0+alpha*pow(x/(lane2*cap2), beta));
-                mac->setParametersForBitrate(BITRATES_80211P[b2]);
-            }
-        } else {
-            Mac1609_4 *mac = FindModule<Mac1609_4*>::findSubModule(getParentModule());
+    double flow1 = traci->getLastStepVehicleNumber("flow1a")+traci->getLastStepVehicleNumber("flow1b");//+traci->getLastStepVehicleNumber("flow1c");
+    double flow2 = traci->getLastStepVehicleNumber("flow2");
 
-            if( J1 <= J2) {//choose route 1
-                traciVehicle->changeRoute("edge2", 9999);
-                double u = flow1/l1;
-                x = u*v1*3600;
-                speed = v_max/(1.0+alpha*pow(x/(lane1*cap1), beta));
-                traciVehicle->setSpeed(speed);
-                mac->setParametersForBitrate(BITRATES_80211P[b1]);
-            } else {//choose route 2
-                traciVehicle->changeRoute("edge1", 9999);
-                double u = flow2/l2;
-                x = u*v2*3600;
-                speed = v_max/(1.0+alpha*pow(x/(lane2*cap2), beta));
-                traciVehicle->setSpeed(speed);
-                mac->setParametersForBitrate(BITRATES_80211P[b2]);
-            }
+    TraCICommandInterface::Road road1 = TraCICommandInterface::Road(traci, "edge1");
+    TraCICommandInterface::Road road2 = TraCICommandInterface::Road(traci, "edge2");
+
+    double rate = 4000.0, l1 = 1000, l2 = 500, v_max = 15.65, cap1 = 1500, cap2 = 1500, alpha=0.2, beta=10, lane1 = 2, lane2 = 1;
+    double weight = 0.2;
+    
+    double r = 361.0;
+    double p = 1.0/20.0;
+    
+    auto c = getCommunicationCost();
+    double c1 = c.first;
+    double c2 = c.second;
+    
+    double v1 = road1.getMeanSpeed();
+    if(v1 == 0.0) v1 = 0.01;
+    double v2 = road2.getMeanSpeed();
+    if(v2 == 0.0) v2 = 0.01;
+    double t1 = l1/v1;
+    double t2 = l2/v2;
+    double x1 = flow1/l1*v1;
+    double x2 = flow2/l2*v2;
+    
+    double J1 = weight*t1+(1-weight)*c1;
+    double J2 = weight*t2+(1-weight)*c2;
+    double systemCost = t1*x1+t2*x2;
+    
+    output << simTime() <<" " << systemCost << "\n";
+    outputFlow << simTime() <<" "<<x1*3600<<"\n";
+    
+    double x, speed;
+    if(simTime() < 20) {
+        Mac1609_4 *mac = FindModule<Mac1609_4*>::findSubModule(getParentModule());
+        if(simTime() > 20) {//choose route 1
+            traciVehicle->changeRoute("edge2", 9999);
+            double u = flow1/l1;
+            x = u*v1*3600;
+            mac->setParametersForBitrate(BITRATES_80211P[b1]);
+        } else {//choose route 2
+            traciVehicle->changeRoute("edge1", 9999);
+            double u = flow2/l2;
+            x = u*v2*3600;
+            mac->setParametersForBitrate(BITRATES_80211P[b2]);
         }
-        std::cout << simTime() <<": "<< c1 <<" " << c2 <<" "<< t1<< " "<<t2 <<" " << x1*3600 << " " << x2*3600 << "\n";
+    } else {
+        Mac1609_4 *mac = FindModule<Mac1609_4*>::findSubModule(getParentModule());
+        
+        if( J1 <= J2) {//choose route 1
+            traciVehicle->changeRoute("edge2", 9999);
+            double u = flow1/l1;
+            x = u*v1*3600;
+            speed = v_max/(1.0+alpha*pow(x/(lane1*cap1), beta));
+            mac->setParametersForBitrate(BITRATES_80211P[b1]);
+        } else {//choose route 2
+            traciVehicle->changeRoute("edge1", 9999);
+            double u = flow2/l2;
+            x = u*v2*3600;
+            mac->setParametersForBitrate(BITRATES_80211P[b2]);
+        }
+    }
+    std::cout << simTime() <<": "<< c1 <<" " << c2 <<" "<< t1<< " "<<t2 <<" " << x1*3600 << " " << x2*3600 << "\n";
 }
 
 void TraCIDemo11p::onWSA(WaveServiceAdvertisment* wsa) {
@@ -121,13 +116,11 @@ void TraCIDemo11p::onWSA(WaveServiceAdvertisment* wsa) {
 }
 
 void TraCIDemo11p::onWSM(WaveShortMessage* wsm) {
-    findHost()->getDisplayString().updateWith("r=16,green");
     std::string msg = wsm->getWsmData();
-
     if (mobility->getRoadId()[0] != ':' && msg.size() > 4) {
         if (msg.substr(0,4)=="req@") {
             std::string req = msg.substr(4);
-            int prob = rand()%10;
+            int prob = rand()%20;
             if (prob==1) {
                 WaveShortMessage* wsm1 = new WaveShortMessage();
                 std::string str = "content@"+std::to_string(wsm->getSenderAddress());
@@ -137,8 +130,8 @@ void TraCIDemo11p::onWSM(WaveShortMessage* wsm) {
             }
         } else if (msg.size() >= 8 && msg.substr(0,8)=="content@") {
             if (std::to_string(myId) != msg.substr(8)) return;
-                BaseWaveApplLayer::hitMap[simTime()].insert(std::make_pair(myId, mobility->getRoadId()));
-                emit(throughputSignal, long(wsm->getBitLength()));
+            BaseWaveApplLayer::hitMap[simTime()].insert(std::make_pair(myId, mobility->getRoadId()));
+            emit(throughputSignal, long(wsm->getBitLength()));
         } else {
             std::cout<<myId<<" receive data "<< msg << "\n";
         }
@@ -167,15 +160,15 @@ void TraCIDemo11p::handleSelfMsg(cMessage* msg) {
 
 void TraCIDemo11p::handlePositionUpdate(cObject* obj) {
     BaseWaveApplLayer::handlePositionUpdate(obj);
-
+    
     // stopped for for at least 10s?
     if (simTime() - lastDroveAt >= 2 && sentMessage == false) {
         sentMessage = true;
-
+        
         WaveShortMessage* wsm = new WaveShortMessage();
         populateWSM(wsm);
         wsm->setWsmData(mobility->getRoadId().c_str());
-
+        
         //host is standing still due to crash
         if (dataOnSch) {
             startService(Channels::SCH2, 42, "Traffic Information Service");
